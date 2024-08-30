9b9eba378e11eb9ba0d7e9d6f84446531713fb13
nhatt <nhattuan44t@gmail.com>
2024-08-30 14:03:41 +0700

    ok

--- finance/settings.py
+++ finance/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
