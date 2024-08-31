4bb84d37d85046cfccbb178a370d001c994d195e
nhatt <nhattuan44t@gmail.com>
2024-08-31 11:43:07 +0700

    ok

--- staticfiles/js/login.js
+++ staticfiles/js/login.js
@@ -9,6 +9,23 @@ function loginByEnter()
         login();
     }
 }
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Tìm cookie có tên "name"
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
 function login()
 {
     const xhttp = new XMLHttpRequest();
@@ -35,7 +52,10 @@ function login()
         password:document.getElementById("Password").value
     }
     postData=JSON.stringify(userInfo)
+    var csrftoken = getCookie('csrftoken');
+
     xhttp.open("POST", "/api/v1/login",false);
-    xhttp.setRequestHeader("Content-type","application/json")
+    xhttp.setRequestHeader("Content-type","application/json");
+    xhr.setRequestHeader('X-CSRFToken', csrftoken);
     xhttp.send(postData)
 }
