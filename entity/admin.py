803a5e5f8d20aa71e0ffebf4c94abd6083797014
nhatt <nhattuan44t@gmail.com>
2024-08-31 10:38:14 +0700

    ok

---
+++ entity/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import User,Finance,Human
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Finance)
+admin.site.register(Human)
+
--- entity/models.py
+++ entity/models.py
@@ -4,8 +4,12 @@ class User(models.Model):
     username = models.CharField(max_length=225,unique=True)
     password = models.CharField(max_length=225)
     role = models.CharField(max_length=225)
+    def __str__(self):
+        return self.username
 class Human(models.Model):
     humanName = models.CharField(max_length=225)
+    def __str__(self):
+        return self.humanName
 class Finance(models.Model):
     humanName = models.ForeignKey(Human,on_delete=models.CASCADE,null=True)
     type = models.CharField(max_length=225)
@@ -16,3 +20,4 @@ class Finance(models.Model):
     endAt = models.DateField(null=True)
     deadline =models.DateField(null=True)
     user =models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    
