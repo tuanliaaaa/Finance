da9fc3d493d7d3b1a28b4281c8ec110a5ae0baa4
tuanliaaaa <nhattua44t@gmail.com>
2025-03-19 11:28:16 +0000

    tuan

--- api/views.py
+++ api/views.py
@@ -11,8 +11,8 @@ from django.utils.decorators import method_decorator
 import datetime
 class Login(APIView):
     def post(self,request):
-        exp=datetime.datetime.now(tz=timezone.utc) + timedelta(minutes=50)
-        userRequestToken =request.data     
+        exp=datetime.datetime.now(tz=timezone.utc) + timedelta(minutes=500000)
+        userRequestToken =request.data
         if 'username' not in userRequestToken or not userRequestToken['username']:
             return Response({"message":"Vui lòng nhập username"},400)
         if 'password' not in userRequestToken or not userRequestToken['password']:
@@ -22,7 +22,7 @@ class Login(APIView):
            return Response({"message":"mật khẩu sai rồi thằng ngu"},400)
         user=users[0]
         payLoad = {'UserID':user.id,"UserName":user.username,"Role":user.role,"exp":exp}
-        jwtData = jwt.encode(payLoad,SECRET_KEY,) 
+        jwtData = jwt.encode(payLoad,SECRET_KEY,)
         jwtUser={"access":jwtData}
         return Response(CustomResponse(200,"success",jwtUser).to_dict(),201)
 class Humen(APIView):
@@ -39,7 +39,7 @@ class Finances(APIView):
     @method_decorator(RoleRequest(allowed_roles=['user',]))
     def get(self,request):
         finances = Finance.objects.select_related('humanName').filter(user_id=request.UserID,status=request.GET.get('status'))
-        
+
         # Tạo danh sách các đối tượng với thông tin Finance và Human
         financesJson = [
             {
@@ -65,4 +65,3 @@ class Finances(APIView):
         }, status=200)
 
 
-        
